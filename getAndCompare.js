4babc3f5cbc47af8da8a050f0026e033d8d04efc
Taylor <tfreeman17@gmail.com>
2016-07-26 09:39:28 -0600

    Working on money function

---
+++ getAndCompare.js
@@ -0,0 +1,6 @@
+
+// 
+// $get(http://159.203.175.239:8001/ , value)
+
+// 	$(function(){
+// 		$('input').change(function(){
--- radioButtonNumber.js
+++ radioButtonNumber.js
@@ -1,25 +1,64 @@
 
 	$(function(){
-		$('input').change(function(){
-			var  firstNumber=$('input[name="1"]:checked').val()
-				,secondNumber=$('input[name="2"]:checked').val()
-				,thirdNumber=$('input[name="3"]:checked').val()
-				,forthNumber=$('input[name="4"]:checked').val()
-				,submit=$('button[type="submit"]')
+		$('input').change(function(evt){
+			var firstNumber= $('input[name="1"]:checked').val()
+			var secondNumber= $('input[name="2"]:checked').val()
+			var thirdNumber= $('input[name="3"]:checked').val()
+			var forthNumber= $('input[name="4"]:checked').val()
+			var submit = $('button[type="submit"]')
+			var val = $(evt.target).val()
+			console.log(firstNumber)
+			console.log(secondNumber)
+			console.log(thirdNumber)
+			console.log(forthNumber)
+			var arr = [firstNumber, secondNumber, thirdNumber, forthNumber]
+
+		// $.each($('input[value="'+ val +'"]'), function(i, el){
+		// 	el = $(el);
+				
+		// 		if (! el.is(':checked')){
+		// 			el.attr('disabled','disabled')
+		// 		}
+		// 		else if (':unchecked'){
+		// 			el.attr('enable','disabled')
+		// 		 }
+		// });
+		});
+		function submitButton(){
+			function getNums(results){
+				var array2= results.split(",").map(parseFloat);
+				$("#displayNumbers").html(array2.toString());
+			}
+			bank [0]= bank[0] - 2
+
+			function compare(array2, arr){
+				count = 0
+				for (var i = arr.length + 1; i <= 0; i++) {
+					for (var j = array2.length + 1; j <= 0; j++) {
+						if (arr[i] === array2[j])
+						return count 1
+			}
+			function money(count){
+				if 
+					
+			}
+			}
+			$.get("https://www.random.org/integer-sets/?sets=1&num=4&min=1&max=10&commas=on&sort=on&order=random&format=plain&rnd=new", getNums);
+			var winnings = money(compare(array2, arr));
+			bank[0] = bank [0] = winnings;
+		}
+
+		// $.get("https://www.random.org/integer-sets/?sets=1&num=4&min=1&max=10&commas=on&sort=on&order=random&format=plain&rnd=new", getNums);
+ // }
+		$("button").click(submitButton);
+};
 
 
-			$("input[type='radio']").click(function(){
-      			$("#" + $(this).attr("name")).addClass("selected");
-      			console.log(firstNumber.val())
-				console.log(secondNumber.val())
-				console.log(thirdNumber.val())
-				console.log(forthNumber.val())
-			});
 
 
-			if (firstNumber && secondNumber && thirdNumber && forthNumber)
-				submit.removeAttr('disabled')
-			else
-				submit.attr('disabled','disabled')
-		}).eq(0).trigger('change')
-	})
+
+
+// $("button").click(getNums);
+var bank = [10]
+bank [0]=bank[0]
+// function moneyCount(){}
